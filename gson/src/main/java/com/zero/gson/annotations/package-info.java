89440f4a2fd5860c25d6f9805689a6305e29f36a
zero <zero@andui.com>
2020-07-13 16:21:49 +0800

    init

---
+++ gson/src/main/java/com/zero/gson/annotations/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package provides annotations that can be used with {@link com.mykey.id.component.google.gson.Gson}.
+ * 
+ * @author Inderjeet Singh, Joel Leitch
+ */
+package com.zero.gson.annotations;
